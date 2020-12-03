e3bcac11b822500bc99457a29f4c95cf74f596b7
spheppner <hasibandi@gmx.at>
2020-12-03 19:13:31 +0100

    added files

--- motorcontrol.py
+++ motorcontrol.py
@@ -1,9 +1,9 @@
 import RPi.GPIO as GPIO
 from time import sleep
-
 from RpiMotorLib import RpiMotorLib
 import threading
 import queue
+import math
 
 
 __version__ = "1.0.0"
@@ -97,20 +97,94 @@ class ServoMotor:
         GPIO.cleanup()
 
 
+class Plotter(threading.Thread):
+    def __init__(self):
+        super().__init__()
+        self.xmotor = Nema17MotorHandler(
+            Nema17Motor((14, 15, 18), 20, 21, 100, 200, 0.05)
+        )
+        self.ymotor = Nema17MotorHandler(
+            Nema17Motor((13, 6, 5), 26, 19, 100, 200, 0.05)
+        )
+        self.x = 0
+        self.y = 0
+        self.pendrawing = True
+        self.queue = queue.Queue(maxsize=100)
+        self.running = True
+
+    def run(self):
+        self.xmotor.start()
+        self.ymotor.start()
+        while self.running:
+            c = self.queue.get()
+            ccommand = c["command"]
+            cattributes = c["attributes"]
+            if ccommand == "draw-xy":
+                self.handlePen(True)
+                self.handleMotors(cattributes[0], cattributes[1], 40)
+            elif ccommand == "move-xy":
+                self.handlePen(False)
+                self.handleMotors(cattributes[0], cattributes[1], 100)
+
+    def handleMotors(self, x, y, speed):
+        dx = x - self.x
+        dy = y - self.y
+        if dx == 0:
+            vx = 0
+            vy = speed
+            if dy < 0:
+                a = math.radians(-90)
+            else:
+                a = math.radians(90)
+        else:
+            a = math.atan(dy/dx)
+            vx = math.cos(a) * speed
+            vy = math.sin(a) * speed
+        if vx != 0:
+            self.xmotor.queue.put({"command": "move", "attributes": (dx, vx)})
+        if vy != 0:
+            self.ymotor.queue.put({"command": "move", "attributes": (dy, vy)})
+        self.x += dx
+        self.y += dy
+        print(f"Penlength: {math.sqrt((dy**2+dx**2))} | Alpha: {math.degrees(a)}")
+        print(f"X: {self.x} | Y: {self.y}")
+
+    def handlePen(self, state):
+        if self.pendrawing == state:
+            return
+        if state:
+            self.pendrawing = True
+            print("Pen down")
+        else:
+            self.pendrawing = False
+            print("Pen up")
+
+    def kill(self):
+        self.xmotor.kill()
+        self.ymotor.kill()
+        self.xmotor.join()
+        self.ymotor.join()
+        self.running = False
+
+    def calibrate(self):
+        # TODO: implement calibration process
+        self.x = 0
+        self.y = 0
+
+
 if __name__ == "__main__":
-    hmotor = Nema17MotorHandler(Nema17Motor((14, 15, 18), 20, 21, 100, 200, 0.05))
+    plotter = Plotter()
+    plotter.start()
 
-    hmotor.start()
+    plotter.queue.put({"command": "move-xy", "attributes": (100, 100)})
+    plotter.queue.put({"command": "draw-xy", "attributes": (90, 90)})
+    plotter.queue.put({"command": "draw-xy", "attributes": (140, 90)})
+    plotter.queue.put({"command": "draw-xy", "attributes": (140, 120)})
+    plotter.queue.put({"command": "move-xy", "attributes": (0, 90)})
+    plotter.queue.put({"command": "move-xy", "attributes": (0, 0)})
 
-    hmotor.queue.put({"command": "move", "attributes": (50, 10)})
-    hmotor.queue.put({"command": "move", "attributes": (50, 50)})
-    hmotor.queue.put({"command": "move", "attributes": (100, 50)})
-    hmotor.queue.put({"command": "move", "attributes": (-100, 50)})
-    hmotor.queue.put({"command": "move", "attributes": (-50, 50)})
-    hmotor.queue.put({"command": "move", "attributes": (50, 10)})
-
-    while not hmotor.queue.empty():
+    while not plotter.queue.empty():
         pass
 
-    hmotor.kill()
-    hmotor.join()
+    plotter.kill()
+    plotter.join()
